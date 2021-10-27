f8efb13f3afc5be9dc4a399af7a7f97f3c678f52
medev21 <benmarteen07@gmail.com>
2021-10-26 21:50:19 -0400

    add Formik setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import { Formik } from 'formik';
+import FormDatePicker from './components/FormDatePicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Formik
+        initialValues={{ date: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          alert(JSON.stringify(values))
+        }}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+            setFieldValue,
+          } = props;
+
+          return (
+            <form onSubmit={handleSubmit}>
+              <FormDatePicker name="date" />
+              {/* <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+              >
+                Reset
+              </button> */}
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+
+            </form>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
